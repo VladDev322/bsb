3706c57a36def90fbd40b87a3cadb3f494b3d5e7
Vlad Lepilkin <VladL322@yandex.ru>
2025-01-29 21:33:54 +0300

    feat: add news section

--- src/sctipts/swiperConfig.js
+++ src/sctipts/swiperConfig.js
@@ -7,15 +7,15 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 
 export function initSwiper() {
-  const swiper = new Swiper('.swiper-container', {
+  const swiperParnters = new Swiper('.swiper-container-parnters', {
 
     modules: [Navigation, Autoplay],
 
     loop: true,
 
-    autoplay: {
-      delay: 3000
-    },
+    // autoplay: {
+    //   delay: 3000
+    // },
 
     breakpoints: {
       1920: {
@@ -43,5 +43,38 @@ export function initSwiper() {
     },
   });
 
-  return swiper;
+  const swiperNews = new Swiper('.swiper-container-news', {
+
+    modules: [Navigation, Autoplay],
+
+    loop: true,
+
+    autoplay: {
+      delay: 4000
+    },
+
+    breakpoints: {
+      1170: {
+        slidesPerView: 3,
+      },
+      730: {
+        slidesPerView: 2,
+      },
+      400: {
+        slidesPerView: 1,
+      },
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  return swiperParnters, swiperNews;
 }
