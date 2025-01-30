ecc9486bf1cff020eeb5a51b3e0923cc9260635e
Vlad Lepilkin <VladL322@yandex.ru>
2025-01-30 03:24:50 +0300

    fix: fix form overlay (invisible header)

--- src/sctipts/OverlayForm.js
+++ src/sctipts/OverlayForm.js
@@ -4,6 +4,7 @@ export default class OverlayForm {
     formButton: '[data-js-open-form-button]',
     form: '[data-js-form]',
     overlay: '[data-js-form-overlay]',
+    headerInner: '[data-js-header-inner]'
   };
 
   stateClasses = {
@@ -13,34 +14,38 @@ export default class OverlayForm {
 
   constructor() {
     this.rootElement = document.querySelector(this.selectors.root);
-    this.overlayElement = document.querySelector(this.selectors.overlay);
+    this.headerInnerElement = this.rootElement.querySelector(this.selectors.headerInner);
+    this.overlayElement = this.rootElement.querySelector(this.selectors.overlay);
     this.formButtonElement = this.rootElement.querySelector(
       this.selectors.formButton
     );
-    this.form = this.rootElement.querySelector(this.selectors.form);
+    this.formElement = this.rootElement.querySelector(this.selectors.form);
     this.bindEvents();
   }
 
   onformButtonClick = () => {
     this.overlayElement.classList.toggle(this.stateClasses.isActive);
-    this.form.classList.toggle(this.stateClasses.isActive);
+    this.headerInnerElement.classList.add(this.stateClasses.isActive);
+    this.formElement.classList.toggle(this.stateClasses.isActive);
   };
 
   closeForm = (event) => {
     const isClickInsideForm =
-      this.form.contains(event.target) ||
+      this.formElement.contains(event.target) ||
       this.formButtonElement.contains(event.target);
 
     if (!isClickInsideForm) {
       this.overlayElement.classList.remove(this.stateClasses.isActive);
-      this.form.classList.remove(this.stateClasses.isActive);
+      this.headerInnerElement.classList.remove(this.stateClasses.isActive);
+      this.formElement.classList.remove(this.stateClasses.isActive);
     }
   };
 
   closeFormOnEsc = (event) => {
     if (event.key === 'Escape') {
       this.overlayElement.classList.remove(this.stateClasses.isActive);
-      this.form.classList.remove(this.stateClasses.isActive);
+      this.headerInnerElement.classList.remove(this.stateClasses.isActive);
+      this.formElement.classList.remove(this.stateClasses.isActive);
     }
   };
 
